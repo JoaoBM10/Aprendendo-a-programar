d47d98ca90a6640b85e21e7c6fb71adff62778d1
JoaoBM10 <joaobm11@gmail.com>
2024-05-24 10:39:46 -0300

    nova atividade

---
+++ Ex22.py
@@ -0,0 +1,7 @@
+nome = input('Digite seu nome completo:').strip()
+print('Analisando seu nome...')
+print('Seu nome maiúsculo é {}.'.format(nome.upper()))
+print('Seu nome minúsculo é {}.'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras.'.format(len(nome)-nome.count(' ')))
+separar = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras.'.format(separar[0],len(separar[0])))
